609e578c2eec0e85defed4dc4d7389dabbde3c85
Dylan J. Merrell <91173622+Bonezlfc@users.noreply.github.com>
2026-03-05 21:07:56 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 name        'Bonez-Bodycam'
-description 'Bodycam overlay with Axon, Motorola, and Generic styles. Optional night_ers integration for on-shift detection.'
+description 'Bodycam overlay with Axon, Motorola, and Generic styles. night_ers integration for on-shift detection.'
 author      'Bonez Workshop'
 version     '2.0.0'
 
